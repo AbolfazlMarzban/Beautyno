9ea3414b3869d12fb7182251440a6b9e9a099410
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-03-05 17:44:33 +0330

    1401-11-14 tab navigation

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,9 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
+import Ionic from "react-native-vector-icons/Ionicons"
+import {NavigationContainer} from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import homeScreen from './homeScreen';
+import profile from './profile';
+
 
 function home(props) {
+    const Tab = createBottomTabNavigator()
     return (
-        <Text>Home</Text>
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({focused, size, color}) => {
+                        let iconName;
+                        if(route.name == "Home"){
+                            iconName = focused ? "home" : "home-outline"
+                        } else if (route.name == "Profile"){
+                            iconName = focused ? "person-circle-sharp" : "person-circle-outline"
+                        }
+                        return <Ionic name={iconName} size={size} colour={color} />
+                    }
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'black',
+                    inactiveTintColor: 'black',
+                    showLabel: false,
+                    style: {
+                        backgroundColor: '#ffc125',
+                        height: 60,
+                    },
+                }
+                }
+            >
+                <Tab.Screen name="Home" component={homeScreen}></Tab.Screen>
+                <Tab.Screen name="Profile" component={profile}></Tab.Screen>
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 }
 
---
+++ app/screens/homeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function homeScreen(props) {
+    return (
+        <View>
+            <Text>homeScreen</Text>
+        </View>
+    );
+}
+
+export default homeScreen;
